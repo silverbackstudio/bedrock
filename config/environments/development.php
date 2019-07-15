45ed9f2c2e2c9dd08341cb69712c5e9b94ab85af
Brando Meniconi <b.meniconi@silverbackstudio.it>
2019-07-15 09:24:04 +0200

    gae: fix logging in configs

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,15 +1,11 @@
 <?php
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
 /** Development */
-define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
 define('JETPACK_DEV_DEBUG', true);
-
-define( 'WP_POST_REVISIONS', 10 );
+define('WP_POST_REVISIONS', 10 );
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 /**
  * Setup Log Handlers
