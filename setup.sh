8a8b5ddc754fde6a2eca90ac23ac01f8e57ee70d
Brando Meniconi <b.meniconi@silverbackstudio.it>
2020-06-10 10:41:13 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,66 @@
 #! /bin/bash
 # SilverbackStudio AppEngine Project Setup 
-read -p "Project ID: " GCLOUD_PROJECT_ID
-read -p "Project name: " GCLOUD_PROJECT_NAME
+echo "Using Project ID:" $GOOGLE_CLOUD_PROJECT
 
-# Create GCloud Project
-gcloud projects create $GCLOUD_PROJECT_ID --name=$GCLOUD_PROJECT_NAME
+read -p "Domain: " WEBSITE_DOMAIN
+read -p "Billing Account: " BILLING_ACCOUNT
+read -e -p "Cloud Region: " -i "europe-west3" GOOGLE_CLOUD_REGION
+read -e -p "Repo Name: " -i "wp-website" GOOGLE_CLOUD_REPO_NAME
+read -e -p "Cloud SQL instance name: " -i "wp-website" GOOGLE_CLOUD_SQL_NAME
+read -e -p "Database Instance Tier: " -i "db-f1-micro" GOOGLE_CLOUD_SQL_TIER
+read -e -p "Database Name: " -i "wordpress" GOOGLE_CLOUD_SQL_DB_NAME
+read -e -p "Database User: " -i "wordpress_user" GOOGLE_CLOUD_SQL_DB_USER
 
-# Set the project in the flags file 
-echo "      --project: ${GCLOUD_PROJECT_ID}" >> flags.yml
+echo "Creating project in region:" $GOOGLE_CLOUD_REGION
 
 # Enable AppEngine
-gcloud services enable appengine.googleapis.com --project=$GCLOUD_PROJECT_ID
+gcloud services enable appengine.googleapis.com
 
 # Link to billing account
-gcloud alpha billing projects link $GCLOUD_PROJECT_ID --billing-account=003779-F1328B-4143F9
+gcloud beta billing projects link $GOOGLE_CLOUD_PROJECT --billing-account=$BILLING_ACCOUNT
 
 # Set up git repo
-gcloud source repos create website --flags-file=flags.yml
-WEBSITE_REPO_URL=$(gcloud source repos list --flags-file=flags.yml --format="value(url)" --filter="name~website")
+gcloud source repos create $GOOGLE_CLOUD_REPO_NAME
+WEBSITE_REPO_URL=$(gcloud source repos list --format="value(url)" --filter="name~wp-website")
 git remote add gcloud $WEBSITE_REPO_URL
 git remote set-url --push origin no-pushing
 git push --set-upstream gcloud
 
-# Set Project Permissions
-IAM_ETAG=$(gcloud projects get-iam-policy ${GCLOUD_PROJECT_ID} --format="value(etag)")
-echo $IAM_ETAG >> iam.yml
-gcloud set-iam-policy $GCLOUD_PROJECT_ID iam.yml
+DB_ROOT_PASSWORD=$(openssl rand -base64 24)
+DB_USER_PASSWORD=$(openssl rand -base64 24)
+DB_PREFIX=$(openssl rand -base64 2)
+DB_INSTANCE_NAME=$GOOGLE_CLOUD_PROJECT:$GOOGLE_CLOUD_REGION:$GOOGLE_CLOUD_SQL_NAME
 
-# Verify domain
-# gcloud domains verify example.com
+# Create a basic SQL instance
+gcloud sql instances create $GOOGLE_CLOUD_SQL_NAME --root-password=$DB_ROOT_PASSWORD --region=$GOOGLE_CLOUD_REGION --tier=$GOOGLE_CLOUD_SQL_TIER --database-version='MYSQL_5_7' --backup 
+
+echo "Started SQL instance $DB_INSTANCE_NAME with root password: " $DB_ROOT_PASSWORD
+
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+
+./cloud_sql_proxy -dir=/cloudsql -instances=$DB_INSTANCE_NAME=tcp:3307 & SQL_PID=$!
+sleep 5
+mysql -u root --password=$DB_ROOT_PASSWORD -h 127.0.0.1 --port 3307 -e "\
+    CREATE DATABASE $GOOGLE_CLOUD_SQL_DB_NAME; \
+    GRANT ALL ON $GOOGLE_CLOUD_SQL_DB_NAME.* TO '$GOOGLE_CLOUD_SQL_DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" \
+    && echo "Created SQL database with user password: " $DB_USER_PASSWORD
+kill $SQL_PID
+
+rm cloud_sql_proxy
+
+# Replace vars in env
+sed -i -e "s/DB_NAME: ''/DB_NAME: '$GOOGLE_CLOUD_SQL_DB_NAME'/g" env_variables.yaml
+sed -i -e "s/DB_USER: ''/DB_USER: '$GOOGLE_CLOUD_SQL_DB_USER'/g" env_variables.yaml
+sed -i -e "s/DB_PASSWORD: ''/DB_PASSWORD: '$DB_USER_PASSWORD'/g" env_variables.yaml
+sed -i -e "s/DB_HOST: '.*'/DB_HOST: ':\/cloudsql\/$DB_INSTANCE_NAME'/g" env_variables.yaml
+sed -i -e "s/DB_PREFIX: '.*'/DB_PREFIX: 'wp${DB_PREFIX}_'/g" env_variables.yaml
+
+sed -i -e "s/WP_HOME: ''/WP_HOME: 'https:\/\/$WEBSITE_DOMAIN'/g" env_variables.yaml
+sed -i -e "s/WP_SITEURL: ''/WP_SITEURL: 'https:\/\/$WEBSITE_DOMAIN\/wp'/g" env_variables.yaml
+
+# # Create AppEngine App
+gcloud app create --region=$GOOGLE_CLOUD_REGION
+
+# # Verify domain
+gcloud domains verify $WEBSITE_DOMAIN
+
